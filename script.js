067d3bc9f4fa06bd86d07c78979a693721d15f82
Anna Severine jensen <anna@laptop-6tifoktb.kea.local>
2019-09-13 11:45:08 +0200

    knapper og style

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", start);
 const url = "https://spreadsheets.google.com/feeds/list/1IkL13--O3Ree1nbc8UssMo-qGr_9FG60RnvlRYirgdk/od6/public/values?alt=json";
 let alleData;
-let filter = "alle";
+let filter = "forside";
 
 function start() {
     const filterKnapper = document.querySelectorAll("nav button");
-    filterKnapper.forEach(knap => knap.addEventListener("click", filtrerKategorier));
+    filterKnapper.forEach(knap => knap.addEventListener("click", filtrerAlleData));
     document.querySelector("#detalje").style.display = "none";
     skjulDetalje();
     loadData();
 }
 
-function filtrerKategorier() {
-    filter = this.dataset.kategori;
+function filtrerAlleData() {
+    filter = this.dataset.data;
     document.querySelector(".valgt").classList.remove("valgt");
     this.classList.add("valgt");
     vis();
@@ -30,7 +30,7 @@ function vis() {
     const dest = document.querySelector("#mad-liste");
     dest.textContent = "";
     alleData.feed.entry.forEach(data => {
-        if (data.gsx$kategori.$t == filter || filter == "alle") {
+        if (data.gsx$kategori.$t == filter || filter == "forside") {
 
             //klone skabelon
             const klon = skabelon.cloneNode(true);
@@ -40,17 +40,15 @@ function vis() {
             h2.textContent = data.gsx$navn.$t;
 
             const img = klon.querySelector("img");
-            img.src = "billeder/" + data.gsx$billede.$t + ".jpg";
-            img.src = "billeder/" + data.gsx$billede.$t + ".png";
-            img.alt = "Billede af " + data.gsx$navn.$t;
+            img.src = "billeder/" + data.gsx$billede.$t + "";
+            img.src = "billeder/" + data.gsx$billede.$t + "";
+            img.alt = "Billede af" + data.gsx$navn.$t;
 
             const p1 = klon.querySelector("p");
             const p2 = klon.querySelector("p + p");
             const p3 = klon.querySelector("p + p + p");
             const p4 = klon.querySelector("p + p + p + p");
-            p1.textContent = " Pris " + data.gsx$adresse.$t + ",-";
-            p2.textContent += " " + data.gsx$kort.$t;
-            p3.textContent += " Oprindelse: " + data.gsx$bedømmelse.$t;
+
 
             dest.appendChild(klon);
 
@@ -69,11 +67,16 @@ function visDetalje(data) {
     document.querySelector("#detalje .luk").addEventListener("click", skjulDetalje);
 
     document.querySelector("#detalje h2").textContent = data.gsx$navn.$t;
-    document.querySelector("#detalje img").src = "billeder/" + data.gsx$billede.$t + ".jpg";
-    document.querySelector("#detalje img").src = "billeder/" + data.gsx$billede.$t + ".png";
+
+    document.querySelector("#detalje img").src = "billeder/" + data.gsx$billede.$t;
     document.querySelector("#detalje img").alt = `Portræt af ${data.gsx$billede.$t}`;
 
     document.querySelector("#detalje p").textContent = data.gsx$lang.$t;
+
+
+    p1.textContent = " Pris " + data.gsx$adresse.$t + ",-";
+    p2.textContent += " " + data.gsx$kort.$t;
+    p3.textContent += " " + data.gsx$bedømmelse.$t;
     //p4.textContent += " lang " + kategori.gsx$lang.$t;
     //document.querySelector("#detalje p4").textContent = kategori.gsx$lang.$t;
 
