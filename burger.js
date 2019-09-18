31eab8492508629a0f423087218a037e747b48c1
Bp Bpcoholding Co Holding <bpcoholding@Bps-MacBook-Pro.local>
2019-09-18 15:42:38 +0200

    burger menu kommentarer

--- burger.js
+++ burger.js
@@ -1,10 +1,10 @@
 window.addEventListener("load", sidenVises);
-
+//denne funktion gør at burger menuen åbner og lukkerr
 function sidenVises() {
     console.log("sidenVises");
     document.querySelector("#menuknap").addEventListener("click", toggleMenu);
 }
-
+//denne funktion skjuler og åbner burger menuen
 function toggleMenu() {
     console.log("toggleMenu");
     document.querySelector("#menu").classList.toggle("hidden");
